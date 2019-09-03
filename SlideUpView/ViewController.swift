319e961e6fea6c8a2a1a58f452aa9b343500a54a
dks333 <kaishan333sam@gmail.com>
2019-09-02 21:23:43 -0700

    First

---
+++ SlideUpView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideUpView
+//
+//  Created by Sam Ding on 9/2/19.
+//  Copyright © 2019 Kaishan Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
